e1e15eac1152e1f5d237458f9281cae06f364842
Ben Weber <bweber89@gmail.com>
2017-01-28 20:46:57 +0100

    Adds CORS

--- server.js
+++ server.js
@@ -3,6 +3,12 @@
 const app = require('express')();
 let port = process.env.PORT || 8080;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(require('./controllers'));
 
 app.listen(port, () => {
